6e2d2c3b18d0a6a93a311ed8a20ebc71e544bb9d
RobbieClarken <robbie.clarken@gmail.com>
2016-05-05 22:35:26 +1000

    Make spectrum waveform show random values

--- ioc/pyepicsApp/src/spectrum.c
+++ ioc/pyepicsApp/src/spectrum.c
@@ -3,11 +3,19 @@
 #include <registryFunction.h>
 #include <epicsExport.h>
 
+#include <stdlib.h>
+
+
+static float randomFloatFromRange (float min, float max) {
+  float v = (float)rand()/(float)(RAND_MAX);
+  return (max - min) * v + min;
+}
+
 
 static long processSpectrum (aSubRecord* record) {
   float* vala = (float*)record->vala;
   for (long i = 0; i < record->nova; i++) {
-    vala[i] = (float)i;
+    vala[i] = randomFloatFromRange(5, 10);
   }
   return 0;
 }
